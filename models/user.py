0f3fe29530fa7b5622fff9bb52bb2bc45b1a16c7
naylazaky <naylaazakyfauziah@gmail.com>
2026-04-15 19:40:01 +0800

    feat: tambahkan model User dan Queue dengan relasi One-to-Many

---
+++ models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String, nullable=False)
+
+    items = relationship("Queue", back_populates="user")
